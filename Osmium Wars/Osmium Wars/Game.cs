60f07d96fb5ead42c714298e47e8f16aa31babcb
Kai Sellgren <kaisellgren@gmail.com>
2012-02-04 19:23:00 +0200

    Added a soldier.

--- Osmium Wars/Osmium Wars/Game.cs
+++ Osmium Wars/Osmium Wars/Game.cs
@@ -14,12 +14,29 @@ namespace Osmium_Wars
     /// <summary>
     /// This is the main type for your game
     /// </summary>
-    public class Game1 : Microsoft.Xna.Framework.Game
+    public class Game : Microsoft.Xna.Framework.Game
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
-        public Game1()
+        // Set the 3D model to draw.
+        Model myModel;
+
+        // The aspect ratio determines how to scale 3d to 2d projection.
+        float aspectRatio;
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            using (Game game = new Game())
+            {
+                game.Run();
+            }
+        }
+
+        public Game()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
@@ -47,7 +64,10 @@ namespace Osmium_Wars
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
+            myModel = Content.Load<Model>("Units/Player");
+            aspectRatio = graphics.GraphicsDevice.Viewport.AspectRatio;
+            graphics.PreferMultiSampling = true;
+            graphics.ApplyChanges();
         }
 
         /// <summary>
@@ -67,13 +87,20 @@ namespace Osmium_Wars
         protected override void Update(GameTime gameTime)
         {
             // Allows the game to exit
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
                 this.Exit();
 
-            // TODO: Add your update logic here
+            modelRotation += (float)gameTime.TotalGameTime.Seconds / 100;
 
             base.Update(gameTime);
         }
+
+        // Set the position of the model in world space, and set the rotation.
+        Vector3 modelPosition = Vector3.Zero;
+        float modelRotation = 0.0f;
+
+        // Set the position of the camera in world space, for our view matrix.
+        Vector3 cameraPosition = new Vector3(0.0f, 850.0f, 300.0f);
 
         /// <summary>
         /// This is called when the game should draw itself.
@@ -83,7 +110,26 @@ namespace Osmium_Wars
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
+            // Copy any parent transforms.
+            Matrix[] transforms = new Matrix[myModel.Bones.Count];
+            myModel.CopyAbsoluteBoneTransformsTo(transforms);
+
+            // Draw the model. A model can have multiple meshes, so loop.
+            foreach (ModelMesh mesh in myModel.Meshes)
+            {
+                // This is where the mesh orientation is set, as well 
+                // as our camera and projection.
+                foreach (BasicEffect effect in mesh.Effects)
+                {
+                    effect.EnableDefaultLighting();
+                    effect.AmbientLightColor = new Vector3(255, 255, 255);
+                    effect.World = transforms[mesh.ParentBone.Index] * Matrix.CreateScale(0.2f, 0.2f, 0.2f) * Matrix.CreateRotationY(modelRotation) * Matrix.CreateTranslation(modelPosition);
+                    effect.View = Matrix.CreateLookAt(cameraPosition, Vector3.Zero, Vector3.Forward);
+                    effect.Projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(45.0f), aspectRatio, 1.0f, 10000.0f);
+                }
+                // Draw the mesh, using the effects set above.
+                mesh.Draw();
+            }
 
             base.Draw(gameTime);
         }
